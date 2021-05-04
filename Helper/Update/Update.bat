9e37eebae8661eb1c8fcf216afb67ccf89f7e367
BenjaminHalko <benjamin.g.halko@gmail.com>
2021-05-04 09:56:52 -0700

    Update Update.bat

--- Helper/Update/Update.bat
+++ Helper/Update/Update.bat
@@ -5,7 +5,6 @@ cd..
 cd..
 cd..
 
-robocopy "%CD%%1" "%CD%" /MIR /XF settings.ini /XD "%CD%%1" .git
+robocopy "%CD%%1" "%CD%" /E /MIR /XF settings.ini /XD "%CD%%1" .git
 
-rm WiiMusicEditor.zip
 python WiiMusicEditor.py
